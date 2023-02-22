2afa5b9af8ce7d10cc552463a6718cef6eac6c80
donwung <113479299+donwung@users.noreply.github.com>
2023-02-22 10:10:49 -0800

    minor refactor, added comments

--- 4chscraper.py
+++ 4chscraper.py
@@ -8,48 +8,33 @@ import shutil
 import sys
 app = Flask(__name__)
 
-
+# reads url, puts it into soup, returns all hrefs ie web files
 def parse_url(thread_URL):
-
     req = urllib.request.Request(
         thread_URL, headers={'User-Agent': 'Mozilla/5.0'})
     response = urllib.request.urlopen(req)
-    f = open('C:\\Users\\John\\Desktop\\Programming\\Projects\\4chscraper\\html_in.html', encoding="utf-8")
     html_doc = response.read()
-    print(response)
-    f.close()
-
+    # print(response)
     soup = BeautifulSoup(html_doc, 'html.parser')
-
     links = soup.find_all("a", class_="fileThumb")
-
     return links
 
-
-# print(resource)
-# content = resource.read().decode(resource.headers.get_content_charset())
-
+# downloads file into static folder
 def download(url, file_name):
     with open("./static/"+file_name, "wb") as file:
         res = get(url)
-        # this writes the content
         file.write(res.content)
 
-        # TODO: delete
-        # os.remove("temp/1676914568692510.png")
-
-
+# after getting a regex match array, this flattens an array of characters 
 def flattenPostNumArr(arr):
     res = ""
     for i in range(1, len(arr)):
         res = res + arr[i]
     return res
 
-
 @app.route("/")
 def index():
     return render_template("index.html")
-
 
 @app.route("/get_imgs", methods=["POST"])
 def get_imgs():
@@ -70,21 +55,15 @@ def get_imgs():
         fileExt = re.findall("\.[a-z]+", _link)
         fileExt = fileExt[1]
 
-        # this 
-        # TODO: refactor this
-        hosting = re.sub(f"{postNum}{fileExt}", "", _link)
-        # print(hosting)
-
         oneFile = [postNum, fileExt]
         allImgs.append(oneFile)
         fileName = postNum + fileExt
 
-        # this enables a download
+        # this downloads one file named as the post number
         if not os.path.exists(f"./static/{fileName}"):
             download(_link, fileName)
 
     return render_template("out.html", allImgs=allImgs)
-    # return render_template("out.html")
 
 
 @app.route("/keep_selected", methods=["POST"])
@@ -92,49 +71,10 @@ def keep_selected():
     data = {
         **request.form
     }
-    print(f"data to keep: {data}")
-    # deletes static folder and rebuilds static folder
-    # shutil.rmtree("./static")
-    # # print(hosting)
-    # if not os.path.exists("./static"):
-    #     os.makedirs("./static")
 
     for file in os.listdir("./static"):
         print(file)
         if file  not in data.keys():
             os.remove(f"./static/{file}")
             
-
-    # for fileName in data.keys():
-    #     print(fileName)
-
-    #     if os.path.exists(f"./static/{fileName}"):
-    #         # TODO: deletes at name
-    #         os.remove(f"./static/{fileName}")
-    #         # download(_link, fileName)
-
-        # postNum = re.findall("[0123456789]", _link)
-        # postNum = flattenPostNumArr(postNum)
-
-        # fileExt = re.findall("\.[a-z]+", _link)
-        # fileExt = fileExt[1]
-
-        # # TODO: refactor this
-        # hosting = re.sub(f"{postNum}{fileExt}", "", _link)
-        # # print(hosting)
-
-        # oneFile = [postNum, fileExt]
-        # allImgs.append(oneFile)
-        # fileName = postNum + fileExt
-
-    # deletes static folder and rebuilds static folder
-    # shutil.rmtree("./static")
-    # # print(hosting)
-    # if not os.path.exists("./static"):
-    #     os.makedirs("./static")
-
-    # for link in data.keys():
-    #     print(link)
-        # download(hosting+link, link)
     return redirect("/")
-# add a route to trigger a request to start downloading
